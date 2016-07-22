0ee7ebf7a769cdfc8f31db985faeb74251467498
number5wh <1031158073@qq.com>
2016-07-22 16:32:44 +0800

    设置模板

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+<link rel="stylesheet" href="{{url('css/book.css')}}">
+<link rel="stylesheet" href="{{url('css/weui.css')}}">
+<body>
+@yield('body')
+</body>
+<script style="text/javascript" src="{{url('js/jquery-1.11.2.min.js')}}"></script>
+@yield('js')
+</html>
